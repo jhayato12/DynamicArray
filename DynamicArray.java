6b7cd4e6da7139a5e7f3723a24422bc2918ab045
ayat <56408855+jhayato12@users.noreply.github.com>
2023-11-15 18:06:19 +0100

    Add files via upload

---
+++ DynamicArray.java
@@ -0,0 +1,45 @@
+public class DynamicArray<T> {
+    private T[] array;
+    private int size;
+    private int capacity;
+
+    DynamicArray() {
+        this.size = 0;
+        this.capacity = 1;
+        this.array = (T[]) new Object[capacity];
+    }
+
+    public void add(T element) {
+        if(size == capacity) {
+            T[] temp = (T[]) new Object[capacity * 2];
+
+            for(int i = 0; i < capacity; i++) {
+                temp[i] = array[i];
+            }
+            capacity *= 2;
+            array = temp;
+        }
+        array[size] = element;
+        size++;
+    }
+
+    public void replace(T element, int index) {
+        if(index < size) array[index] = element;
+    }
+
+    public T get(int index) {
+        return array[index];
+    }
+
+    public void pop() {
+        size--;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public int capacity() {
+        return capacity;
+    }
+}
